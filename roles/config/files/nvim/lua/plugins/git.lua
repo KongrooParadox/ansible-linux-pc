a31edd2371d9962dbdc450c33f592117784ebeb8
Guillaume Nanty <kongroo.git@proton.me>
2023-07-22 15:57:02 +0200

    feat: fugitive remaps

--- roles/config/files/nvim/lua/plugins/git.lua
+++ roles/config/files/nvim/lua/plugins/git.lua
@@ -27,7 +27,40 @@ return {
             },
         },
     },
+    {
     'tpope/vim-fugitive',
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+local Robot_Fugitive = vim.api.nvim_create_augroup("Robot_Fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = Robot_Fugitive,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>po", function()
+            vim.cmd.Git('push origin')
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git({'pull',  '--rebase'})
+        end, opts)
+
+        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+        -- needed if i did not set the branch up correctly
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+    end,
+})
+        end
+    },
     'tpope/vim-rhubarb',
 }
 
