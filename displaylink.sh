5ed7bd11e2c182668c0c8e64fbdfc2216fa7cb8d
Guillaume Nanty <kongroo.git@proton.me>
2023-11-15 01:05:26 +0100

    fix: displaylink issue with amd gpu

--- displaylink.sh
+++ displaylink.sh
@@ -14,3 +14,11 @@ svn export https://github.com/AdnanHodzic/displaylink-debian/trunk /tmp/dislayli
 cd /tmp/dislaylink
 sudo ./displaylink-debian.sh --install
 cd ~
+
+sudo rm /etc/X11/xorg.conf.d/20-displaylink.conf
+read -p "Reboot now ? Y/n (defaults to Yes)" reboot_choice
+case $reboot_choice in
+    [Yy]* ) sudo reboot now;;
+    [Nn]* ) exit;;
+    * ) sudo reboot now;;
+esac
